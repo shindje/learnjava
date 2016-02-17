de46d0f22fa4a4d3443d89471fb1f25106127af1
shindje <aleks.shindje@gmail.com>
2016-02-18 00:03:34 +0300

    First commit

---
+++ JavaCore_chapter3/src/DataTypes.java
@@ -0,0 +1,23 @@
+
+public class DataTypes {
+	
+	//int - 4 bytes = 4*32 = 4 billions / 2
+	int i_min = -2_147_483_648;
+	int i_max =  2_147_483_647;
+	
+	//short - 2 bytes = 2*16 = 64 000 / 2
+	short s_min = -32_768;
+	short s_max = 32_767;
+	
+	//long - 8 bytes = 2*64 = 9 223 372 036 854 775 808 * 2
+	long l_min = -9_223_372_036_854_775_808L;
+	long l_max = 9_223_372_036_854_775_807L;
+	
+	//byte - 1 byte = 2*8 = 256 /2
+	byte b_min = -128;
+	byte b_max = 127;
+	
+	int hexadecimal = 0xCAFE;
+	int octal = 010;
+	int binary = 0b0110_0101;
+}
