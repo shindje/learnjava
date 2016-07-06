31133f07ec35731fe667508b5eba9ed501f3a622
a.korotkov <a.korotkov@e-soft.ru>
2016-07-06 18:12:04 +0200

    Operations

---
+++ JavaCore_chapter3/src/Operations.java
@@ -0,0 +1,10 @@
+public class Operations {
+
+	public static void main(String ... s) {
+		System.out.println(-25>>1);	//-13
+		System.out.println(-25>>>1);	//2147483635
+		System.out.println(25>>1);	//12
+		System.out.println(25>>>1);	//12
+
+	}
+}
