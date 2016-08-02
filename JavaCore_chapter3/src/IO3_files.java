c963028eda1450469a81be21dfbf0714ba28b247
a.korotkov <a.korotkov@e-soft.ru>
2016-08-02 16:42:00 +0200

    IO part 2

---
+++ JavaCore_chapter3/src/IO3_files.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+public class IO3_files {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		PrintWriter w = new PrintWriter("somefile");
+		w.print(true);
+		w.print("str");
+		w.print(2);
+		w.close();
+		
+		File f = new File("somefile");
+		if (f.exists()) {
+			Scanner s = new Scanner (f);
+			if (s.hasNext())
+				System.out.println(s.next());	//truestr2
+			s.close();
+		}
+	}
+
+}
