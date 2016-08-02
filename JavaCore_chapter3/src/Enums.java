42ee980917b05e365b7f935b1b368daa179cf848
a.korotkov <a.korotkov@e-soft.ru>
2016-08-02 15:52:22 +0200

    IO 1 part

---
+++ JavaCore_chapter3/src/Enums.java
@@ -0,0 +1,12 @@
+enum Size {SMALL, MEDIuM, LARGE, EXTRA_LARGE};
+
+public class Enums {
+	
+	public static void main(String[] args) {
+		Size s = Size.MEDIuM;
+		
+		System.out.println(s);
+		
+	}
+
+}
---
+++ JavaCore_chapter3/src/IO.java
@@ -0,0 +1,33 @@
+import java.io.Console;
+import java.util.Scanner;
+
+
+public class IO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter string: ");
+		String ss = scan.nextLine();
+		System.out.println("Entered: " + ss);
+		
+		System.out.print("Enter words: ");
+		String s = scan.next();
+		System.out.println("Entered: " + s);
+		
+		System.out.print("Enter number: ");
+		int i = scan.nextInt();
+		System.out.println("Entered: " + i);
+		
+		/*??? cons == null
+		Console cons = System.console();
+		String name = cons.readLine();
+		char[] pwd = cons.readPassword();
+		System.out.println("Entered: name: " + name + " pwd: " + pwd);
+		*/
+	}
+
+}
