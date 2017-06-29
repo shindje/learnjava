3d32404ede2a98eac7d844f2c5661d4a74be6e29
a.korotkov <a.korotkov@e-soft.ru>
2017-06-29 17:08:42 +0300

    AClass

---
+++ JavaCore_chapter5/src/AClass.java
@@ -0,0 +1,21 @@
+package src;
+
+public class AClass {
+
+	/**
+	 * @param args
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		// TODO Auto-generated method stub
+		AClass a  = new AClass();
+		System.out.println(a.getClass().getName());
+		System.out.println(AClass.class.getName());
+		System.out.println(int.class.getName());
+		System.out.println(double[].class.getName());
+		System.out.println("String instance: [" + String.class.newInstance() + "]");
+
+	}
+
+}
