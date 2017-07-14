78344976babf18d9ad96d1490e63dbd73fb48aa9
shindje <aleks.shindje@gmail.com>
2017-07-14 18:37:08 +0300

    Inner + Proxy

---
+++ JavaCore_chapter6/src3/Inner.java
@@ -0,0 +1,46 @@
+package src3;
+
+public class Inner {
+
+	private int i = 4;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Inner i = new Inner();
+		In in  = i.new In();
+		System.out.println(in.getI());
+		System.out.println(i.newIn().getOuterI());
+		System.out.println((new Static()).getJ());
+	}
+	
+	In newIn() {
+		return new In();
+	}
+
+	class In {
+		
+		int i = 5;
+		
+		int getI () {
+			return i;
+		}
+		
+		int getOuterI () {
+			return Inner.this.i;
+		}
+	}
+	
+	
+	static class Static {
+		int j = 7;
+		
+		int getJ(){
+			return j;
+		}
+		
+	}
+	
+}
---
+++ JavaCore_chapter6/src3/ToBeProxied.java
@@ -0,0 +1,8 @@
+package src3;
+
+public interface ToBeProxied {
+
+	static int f = 6;
+	
+	int getF();
+}
