946e00d7009366fd2ac7d5763e34c8b718db3752
shindje <aleks.shindje@gmail.com>
2017-08-25 17:36:47 +0300

    swing initial

---
+++ JavaCore_chapter7_swing/src/Sample.java
@@ -0,0 +1,20 @@
+package src;
+
+import javax.swing.JFrame;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("sd");
+		//EventQueue.invokeLater
+		JFrame f = new JFrame();
+		f.setSize(100, 300);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
