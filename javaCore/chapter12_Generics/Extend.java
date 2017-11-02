7eb2641499e43ee2ef0e0a1599d588c9f4794945
manson <manson@my-k-pc>
2017-11-03 00:40:37 +0300

    Generics extend parameter

---
+++ javaCore/chapter12_Generics/Extend.java
@@ -0,0 +1,12 @@
+package chapter12_Generics;
+
+public class Extend {
+	static <T extends Number> int getInt(T number) {
+		return number.intValue();
+	}
+
+	public static void main(String ...strings ) {
+		System.out.println(Extend.getInt(new Double(4.55)));
+	}
+	
+}
