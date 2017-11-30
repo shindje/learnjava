353fa121acd8ae23a68d9e27c6a1e9d60ea1832e
shindje <aleks.shindje@gmail.com>
2017-11-30 17:40:45 +0300

    ReentrantLock

--- javaCore/chapter14_Threads/Sync.java
+++ javaCore/chapter14_Threads/Sync.java
@@ -1,5 +1,8 @@
 package chapter14_Threads;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Sync {
 	
 	/**
@@ -8,9 +11,18 @@ public class Sync {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyLong myL = new MyLong();
-		(new Thread(new MyRunSync(myL))).start();
-		(new Thread(new MyRunSync(myL))).start();
-		//(new Thread(new MyRunSync(myL))).start();
+		myL.lock = new ReentrantLock();
+		myL.useLock = false;
+		for (int i = 1; i<1000; i++) {
+			(new Thread(new MyRunSync(myL))).start();
+		}
+		System.out.println("with no lock. l = " + myL.l);
+		myL.l = 0;
+		myL.useLock = true;
+		for (int i = 1; i<1000; i++) {
+			(new Thread(new MyRunSync(myL))).start();
+		}
+		System.out.println("with lock. l = " + myL.l);
 	}
 }
 
@@ -26,13 +38,25 @@ class MyRunSync implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		for (int i = 1; i<1000; i ++) {
-			l.l = l.l + 5;
-			System.out.println(Thread.currentThread().getId() + " l = " + l.l + ". " + "i = " + i + ". l-i*5 = " + (l.l-i*5));
+			if (l.useLock)
+				l.lock.lock();
+			try{
+				for (int j = 1; j<10; j++) {
+					l.l = l.l + 5;
+					l.l = l.l - 5;
+				}
+				//System.out.println(Thread.currentThread().getId() + " l = " + l.l);// + ". " + "i = " + i + ". l-i*5 = " + (l.l-i*5));
+			} finally {
+				if (l.useLock)
+					l.lock.unlock();
+			}
 		}
 	}
 	
 }
 
 class MyLong {
-	public long l = 0;	
+	public boolean useLock;
+	public long l = 0;
+	public Lock lock;
 }
