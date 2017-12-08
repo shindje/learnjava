02cffd9ec10c051cac51b6dc82f606b01418e9ac
shindje <aleks.shindje@gmail.com>
2017-12-08 19:48:08 +0300

    add volatile

--- javaCore/chapter14_Threads/Synchro.java
+++ javaCore/chapter14_Threads/Synchro.java
@@ -15,9 +15,9 @@ public class Synchro {
 		MyLong2 myL = new MyLong2();
 		for (int i = 1; i<=1000; i++) {
 			if (i%500 == 0)
-				(new Thread(new MyRunSync(myL, true))).start();
+				(new Thread(new MyRunSyncMethod(myL, true))).start();
 			else
-				(new Thread(new MyRunSync(myL, false))).start();
+				(new Thread(new MyRunSyncMethod(myL, false))).start();
 		}
 		
 		try {
@@ -26,15 +26,52 @@ public class Synchro {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("with synchro. l = " + myL.l);
+		System.out.println("with synchro method. l = " + myL.l);
+		
+		for (int i = 1; i<=1000; i++) {
+			if (i%500 == 0)
+				(new Thread(new MyRunSyncBlock(myL, true))).start();
+			else
+				(new Thread(new MyRunSyncBlock(myL, false))).start();
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("with synchro block. l = " + myL.l);
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("volatile create 50 get");
+		for (int i = 1; i<=50; i++)
+			(new Thread(new MyRunVolatileGet(myL))).start();
+
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("volatile create 1 set");
+		(new Thread(new MyRunVolatileSet(myL))).start();
+	
 	}
 }
 	
-class MyRunSync implements Runnable {
+class MyRunSyncMethod implements Runnable {
 	MyLong2 l;
 	boolean unlocker;
 	
-	public MyRunSync(MyLong2 l, boolean unlocker) {
+	public MyRunSyncMethod(MyLong2 l, boolean unlocker) {
 		// TODO Auto-generated constructor stub
 		this.l = l;
 		this.unlocker = unlocker;
@@ -43,14 +80,67 @@ class MyRunSync implements Runnable {
 	@Override
 	public synchronized void run() {
 		// TODO Auto-generated method stub
-		l.someDo(unlocker);
+		l.someDoSynchroMethod(unlocker);
 	}
 }
 
+class MyRunSyncBlock implements Runnable {
+	MyLong2 l;
+	
+	public MyRunSyncBlock(MyLong2 l, boolean unlocker) {
+		// TODO Auto-generated constructor stub
+		this.l = l;
+	}
+	
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		l.someDoSynchroBlock();
+	}
+}
+
+class MyRunVolatileGet implements Runnable {
+	MyLong2 l;
+	
+	public MyRunVolatileGet(MyLong2 l) {
+		// TODO Auto-generated constructor stub
+		this.l = l;
+	}
+	
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		long i = 0;
+		while (l.volatileGet() == 0 && i<1000000000) {
+			i++;
+		}
+		System.out.println(" i=" + i);
+	}
+}
+
+class MyRunVolatileSet implements Runnable {
+	MyLong2 l;
+	
+	public MyRunVolatileSet(MyLong2 l) {
+		// TODO Auto-generated constructor stub
+		this.l = l;
+	}
+	
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		l.volatileSet(1);
+	}
+}
+
+
+
 class MyLong2 {
 	public long l = 0;
+	Object lockObject = new Object();
+	public volatile long vol = 0;
 	
-	public synchronized void someDo(boolean unlocker) {	//use intrinsicLock
+	public synchronized void someDoSynchroMethod(boolean unlocker) {	//use intrinsicLock
 		try {
 			for (int i = 1; i<1000; i ++) {
 				for (int j = 1; j<10; j++) {
@@ -70,9 +160,29 @@ class MyLong2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			notify();	//might be not safe cause unblocked thread might not be unlock lock depends on check condition  
-			//notifyAll();	//more safety
+			//notify();	//might be not safe cause unblocked thread might not be unlock lock depends on check condition  
+			notifyAll();	//more safety
 			System.out.println(Thread.currentThread().getId() + " notified");
 		}
 	}
+	
+	public void someDoSynchroBlock() {	//use intrinsicLock
+		for (int i = 1; i<1000; i ++) {
+			for (int j = 1; j<10; j++) {
+				synchronized (lockObject) {
+					l = l + 5;
+					l = l - 5;
+				}
+			}
+		}
+	}
+	
+	public void volatileSet(long al) {
+		vol = al;
+	}
+	
+	public long volatileGet() {
+		return vol;
+	}
+
 }
