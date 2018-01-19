9501402ef27d2b49ec959a74420c163ba6c530d8
shindje <aleks.shindje@gmail.com>
2018-01-19 18:27:18 +0300

    AutoSync

---
+++ javaCore/chapter14_Threads/AutoSyncThreads.java
@@ -0,0 +1,43 @@
+package chapter14_Threads;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class AutoSyncThreads {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CyclicBarrier b = new CyclicBarrier(3);
+		for (int i=0; i<3; i++)
+			new Thread(new BarrierRun(b)).start();
+	}
+
+}
+
+class BarrierRun implements Runnable {
+	
+	CyclicBarrier barrier;
+	
+	public BarrierRun(CyclicBarrier barrier) {
+		// TODO Auto-generated constructor stub
+		this.barrier = barrier;
+	}
+	
+	public void run() {
+		try {
+			System.out.println("Thread " + Thread.currentThread().getId());
+			Thread.sleep(Thread.currentThread().getId() * 100);
+			barrier.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Thread " + Thread.currentThread().getId() + " to finish");
+	}
+}
