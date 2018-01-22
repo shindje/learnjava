a8a81f4195ae621be2ec0d66334f3de5a0b66b1e
shindje <aleks.shindje@gmail.com>
2018-01-22 23:29:18 +0300

    88
    77

--- javaCore/chapter14_Threads/AutoSyncThreads.java
+++ javaCore/chapter14_Threads/AutoSyncThreads.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CyclicBarrier;
 
 public class AutoSyncThreads {
 
-	/** test win10 eclipse test updatre 444
+	/** test win10 eclipse test updatre 444 555
 	 * @param args
 	 */
 	public static void main(String[] args) {
