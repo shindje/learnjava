eace5f74d1f08e75855051fea3857cf14cade2df
a.korotkov <a.korotkov@e-soft.ru>
2018-03-22 19:28:57 +0300

    Streams

--- javaCore/chapter_2_01_IO/Streams.java
+++ javaCore/chapter_2_01_IO/Streams.java
@@ -1,10 +1,12 @@
 package chapter_2_01_IO;
 
+import java.io.*;
+
 public class Streams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//InputStream is
 	}
 
 }
